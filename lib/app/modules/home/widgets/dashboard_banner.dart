349e41dfdd102dbe68b9ec679287792d88feff03
Titosy Manankasina <dev.titosy@gmail.com>
2024-12-31 22:35:20 +0300

    Enhance DashboardBanner with Lottie animation and refactor layout for improved design

--- lib/app/modules/home/widgets/dashboard_banner.dart
+++ lib/app/modules/home/widgets/dashboard_banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class DashboardBanner extends StatelessWidget {
   const DashboardBanner({super.key});
@@ -13,38 +14,51 @@ class DashboardBanner extends StatelessWidget {
           color: Theme.of(context).colorScheme.primary,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Text(
-                'Learn the basics of Playing the Guitar',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(context).colorScheme.onPrimary),
+            Positioned(
+              right: 10,
+              child: Lottie.asset(
+                'assets/guitar.json',
+                height: 200,
               ),
             ),
-
-            // button
-            Padding(
-              padding: const EdgeInsets.only(left: 14.0),
-              child: Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                      padding: EdgeInsets.symmetric(horizontal: 32),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Review',
-                      style: TextStyle(color: Theme.of(context).primaryColor),
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Text(
+                    'Learn the basics of Playing the Guitar',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).colorScheme.onPrimary),
                   ),
-                ],
-              ),
+                ),
+
+                // button
+                Padding(
+                  padding: const EdgeInsets.only(left: 14.0),
+                  child: Row(
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.onPrimary,
+                          padding: EdgeInsets.symmetric(horizontal: 32),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Review',
+                          style:
+                              TextStyle(color: Theme.of(context).primaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
