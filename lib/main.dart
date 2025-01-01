515cc64af2612ce7c6ffdf543e2bb662a59d014a
Titosy Manankasina <dev.titosy@gmail.com>
2025-01-01 09:24:11 +0300

    Add Firebase integration with configuration files and update build settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -5,8 +6,15 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/shared/theme.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   SystemChrome.setSystemUIOverlayStyle(kSystemUiOverlayStyle);
 
   runApp(
